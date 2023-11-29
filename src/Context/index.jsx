e4ce08ff425d65b06592e78a644c2b33d393f8f4
Nicolas Marroquin <niko.marroquin7@hotmail.com>
2023-11-29 13:17:26 -0500

    Mostramos los productos que clickeamos en la sección detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,10 +3,21 @@ import { createContext, useState } from 'react';
 export const ShoppingCartContext = createContext() 
 
 export const ShoppingCartProvider = ({children}) => {
+    //Shopping Cart - Increment quantity
     const [count, setCount] = useState(0)
+
+    //Product Detail - Open/close
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    // Product Detail - Show Product
+    const [productToShow, setProductToShow] = useState({
+        title: "",
+        price: "",
+        description: "",
+        images: [],
+    })
 
     return (
         <ShoppingCartContext.Provider value={{
@@ -14,7 +25,9 @@ export const ShoppingCartProvider = ({children}) => {
             setCount,
             openProductDetail,
             closeProductDetail,
-            isProductDetailOpen
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
