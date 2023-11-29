dab27f2863bb58f290f58920d9bea6a368011688
Nicolas Marroquin <niko.marroquin7@hotmail.com>
2023-11-29 11:42:35 -0500

    Agregamos los iconos e importamos la libreria

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom"
 import { useContext } from 'react'
+import { ShoppingCartIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Navbar = () => {
@@ -100,8 +101,11 @@ const Navbar = () => {
                         Sign In
                     </NavLink>
                 </li>
-                <li>
-                    🛒 {context.count}
+                <li className='flex'>
+                <ShoppingCartIcon className='h-6 w-6 text-black'></ShoppingCartIcon> 
+                <div>
+                    {context.count}
+                </div>
                 </li>
             </ul>
         </nav>
