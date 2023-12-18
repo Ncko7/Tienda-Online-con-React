8420c583d85fb8a22e6dc3ac19a60c4915f95e64
Nicolas Marroquin <niko.marroquin7@hotmail.com>
2023-12-17 19:01:14 -0500

    Página de MyOrder: órden individual

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -7,6 +7,9 @@ import Layout from "../../Components/Layout"
 
 function MyOrder() {
     const context = useContext(ShoppingCartContext)
+    const currentPath = window.location.pathname
+    let index = currentPath.substring(currentPath.lastIndexOf('/') + 1)
+    if(index === 'last') index = context.order?.length - 1
 
     return (
         <Layout>
@@ -16,9 +19,9 @@ function MyOrder() {
                 </Link>
                 <h1>My Order</h1>
             </div>  
-            
+            <div className='flex flex-col w-80'>
             {
-                context.order?.slice(-1)[0].products.map(product => (
+                context.order?.[index]?.products.map(product => (
                 <OrderCard 
                     key={product.id}
                     id={product.id}
@@ -28,6 +31,8 @@ function MyOrder() {
                 />
                 ))
             }
+            </div>
+            
         </Layout>
     )
 }
